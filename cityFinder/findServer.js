eb0b6e3c1dbc54b3ad76aa140e5a5bbc8e2d7c9c
Shureck <Sashalev200149@gmail.com>
2020-11-29 02:39:39 +0300

    Add findServer

---
+++ cityFinder/findServer.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const url = require('url');
+const data = require('./data.json');
+
+function fix(coord) {
+    if(Array.isArray(coord[0])) {
+        coord = coord.map(fix);
+    } else if (coord.length == 2) {
+        coord = coord.reverse();
+    } else {
+        throw "IDK";
+    }
+
+    return coord;
+}
+
+function getCityCoord(name) {
+    const coordinates = data.find(it => it.name === name).geo.coordinates;
+    return fix(coordinates);
+}
+
+http.createServer(function(request, response){
+    const query = url.parse(request.url,true).query;
+    response.end(JSON.stringify(getCityCoord(query.name)));
+}).listen(3000);
